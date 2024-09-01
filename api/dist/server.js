f33fe6063ad14668e7ef6104fba3bb04fc64ed8d
SaidARSLAN <lost.myreligion@hotmail.com>
2024-09-01 19:14:37 +0300

    [developing] Added save route for saving expense and income

--- api/dist/server.js
+++ api/dist/server.js
@@ -3,19 +3,25 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-// Import the 'express' module along with 'Request' and 'Response' types from express
 const express_1 = __importDefault(require("express"));
-// Create an Express application
 const app = (0, express_1.default)();
-// Specify the port number for the server
-const port = 3000;
-// Define a route for the root path ('/')
+const port = 1283;
 app.get("/", (req, res) => {
-    // Send a response to the client
-    res.send("Hello, TypeScript + Node.js + Express!");
+    res.send("Fetched All Data");
 });
-// Start the server and listen on the specified port
+app.use(express_1.default.json()); // Middleware to parse JSON bodies
+app.post("/save", (req, res) => {
+    const product = req.body.product;
+    const date = req.body.date;
+    const amount = req.body.amount;
+    const description = req.body.description;
+    res.status(200).json({
+        product: product,
+        date: date,
+        amount: amount,
+        description: description,
+    });
+});
 app.listen(port, () => {
-    // Log a message when the server is successfully running
     console.log(`Server is running on http://localhost:${port}`);
 });
--- api/src/server.ts
+++ api/src/server.ts
@@ -1,20 +1,34 @@
-// Import the 'express' module along with 'Request' and 'Response' types from express
 import express, { Request, Response } from "express";
 
-// Create an Express application
 const app = express();
 
-// Specify the port number for the server
-const port: number = 3000;
+const port: number = 1283;
 
-// Define a route for the root path ('/')
-app.get("/", (req: Request, res: Response) => {
-  // Send a response to the client
-  res.send("Hello, TypeScript + Node.js + Express!");
+app.get("/", (_: Request, res: Response) => {
+  res.send("Fetched All Data");
 });
 
-// Start the server and listen on the specified port
+app.use(express.json());
+
+app.post("/save", (req: Request, res: Response) => {
+  const product: string = req.body.product;
+  const date: string = req.body.date;
+  const amount: number = req.body.amount;
+  const description: string = req.body.description;
+
+  res.status(200).json({
+    product: product,
+    date: date,
+    amount: amount,
+    description: description,
+  });
+});
+
+const validateBeforeProcess = (body: object, ROUTE_TYPE: string) => {
+  if (ROUTE_TYPE === "CREATE") {
+  }
+};
+
 app.listen(port, () => {
-  // Log a message when the server is successfully running
   console.log(`Server is running on http://localhost:${port}`);
 });
