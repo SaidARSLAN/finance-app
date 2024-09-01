8b0f5e9df9e794bffe58523b1ddcc0348a2e0055
SaidARSLAN <lost.myreligion@hotmail.com>
2024-09-02 00:03:12 +0300

    [developing] added validation function for start

--- api/src/server.ts
+++ api/src/server.ts
@@ -10,23 +10,42 @@ app.get("/", (_: Request, res: Response) => {
 
 app.use(express.json());
 
-app.post("/save", (req: Request, res: Response) => {
-  const product: string = req.body.product;
-  const date: string = req.body.date;
-  const amount: number = req.body.amount;
-  const description: string = req.body.description;
+app.post("/save", async (req: Request, res: Response) => {
+  try {
+    const isValid = await validateBeforeProcess(req.body, "CREATE");
+    if (isValid.length > 0) {
+      res.status(400).json(JSON.stringify(isValid));
+    }
+    const product: string = req.body.product;
+    const date: string = req.body.date;
+    const amount: number = req.body.amount;
+    const description: string = req.body.description;
 
-  res.status(200).json({
-    product: product,
-    date: date,
-    amount: amount,
-    description: description,
-  });
+    res.status(200).json({
+      product: product,
+      date: date,
+      amount: amount,
+      description: description,
+    });
+  } catch (error: any) {
+    res.status(404).send(error.message);
+  }
 });
 
-const validateBeforeProcess = (body: object, ROUTE_TYPE: string) => {
+const validateBeforeProcess = async (
+  body: { product: string; date: string; amount: number; description: string },
+  ROUTE_TYPE: string,
+) => {
+  const errors = [];
   if (ROUTE_TYPE === "CREATE") {
+    if (!body.product || !body.date || !body.amount || !body.description) {
+      errors.push({
+        type: "CREATE",
+        description: "All data has to be defined!",
+      });
+    }
   }
+  return errors;
 };
 
 app.listen(port, () => {
